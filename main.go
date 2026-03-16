0b1bf42373296c723b7880fa0bbac8bbcbcfc0e2
Bulat <bulat2659@gmail.com>
2026-03-16 09:00:01 +0300

    add scan

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("ToDO CLI started")
 	fmt.Println("Type 'help' to see available command")
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Println("Enter the command: ")
+	scanner.Scan()
 }
